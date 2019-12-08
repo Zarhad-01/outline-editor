b036e17562ea91efc65f20a4cd3fa29753f1769b
Zahid <55988827+Zarhad-01@users.noreply.github.com>
2019-12-08 21:41:19 +0000

    Got the server to run. dunno what it does tho

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const port = process.env.PORT || 8080;
+
 const express = require("express");
 const http = require("http");
 const ws = require("ws");
@@ -10,6 +12,7 @@ const app = express();
 const server = http.createServer(app);
 
 // do websocket stuff at some point
+const wss = new ws.Server({server: server});
 
 
 app.use(express.static(`${__dirname}/webpages`));
